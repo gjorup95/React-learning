16acbf9bf20fb4866410099ba428f180abb59861
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-04-26 14:55:50 +0200

    added faker

--- src/index.js
+++ src/index.js
@@ -5,14 +5,26 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-	return <div>Hi there!</div>;
+	return (
+		<div className="ui cointainer comments">
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:00PM </span>
+					</div>
+					<div className="text">Nice blog post!</div>
+				</div>
+			</div>
+		</div>
+	);
 };
-// Variation
-/* ARROW FUNCTION
-const App = () => {
-	return <div>Hi there!</div>;
-};
-*/
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App tab="home" />);
