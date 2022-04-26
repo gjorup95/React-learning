949950e48301ac2adad6deb544f1a888c28f7f43
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-04-26 15:53:48 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,24 +3,29 @@
 // take the react component and show it on the screen
 import React from "react";
 import { createRoot } from "react-dom/client";
-
+import faker from "@faker-js/faker";
+import CommentDetail from "./CommentDetail";
 const App = () => {
 	return (
 		<div className="ui cointainer comments">
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:00PM </span>
-					</div>
-					<div className="text">Nice blog post!</div>
-				</div>
-			</div>
+			<CommentDetail
+				author="Sam"
+				timeAgo="today at 3:42PM"
+				textMessage="Nej hvor dejligt"
+				profileImage={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author="Alex"
+				timeAgo="yesterday at 3:33PM"
+				textMessage="Det går super fint"
+				profileImage={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author="Jane"
+				timeAgo="today at 1:15PM"
+				textMessage="Hej hvordan går det"
+				profileImage={faker.image.avatar()}
+			/>
 		</div>
 	);
 };
@@ -28,3 +33,4 @@ const App = () => {
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App tab="home" />);
+//
