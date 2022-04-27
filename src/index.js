6a15e85462e4be0e78cfffa74e11b70f89af4053
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-04-27 13:16:22 +0200

    prepared for new app

--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import SeasonDisplay from "./SeasonDisplay";
 import Loader from "./Loader";
 class App extends React.Component {
-	state = { lat: null, errorMessage: "" };
-	componentDidMount() {
-		window.navigator.geolocation.getCurrentPosition(
-			(position) => this.setState({ lat: position.coords.latitude }),
-			(err) => this.setState({ errorMessage: err.message })
-		);
-	}
 	componentDidUpdate() {}
 	// React requires a render function
-	renderContent() {
-		if (this.state.errorMessage && !this.state.lat) {
-			return <div>Error: {this.state.errorMessage}</div>;
-		}
-		if (!this.state.errorMessage && this.state.lat) {
-			return (
-				<div>
-					<SeasonDisplay lat={this.state.lat} />
-				</div>
-			);
-		}
-		return <Loader message="Please accept location request :)" />;
-	}
+	renderContent() {}
 
 	render() {
-		return <div className="border red">{this.renderContent()}</div>;
+		return <div></div>;
 	}
 }
 const container = document.getElementById("root");
