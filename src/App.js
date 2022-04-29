560f0a799e8f8aad5e490d6fbe92028039693951
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-04-29 16:27:42 +0200

    fixed redux store functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
-import React, { useState, useEffect } from "react";
-import SearchBar from "./SearchBar";
-import VideoList from "./VideoList";
-import VideoDetail from "./VideoDetail";
-import useVideos from "../hooks/useVideos";
-const App = () => {
-	const [selectedVideo, setSelectedVideo] = useState(null);
-	const [videos, search] = useVideos("buildings");
-
-	useEffect(() => {
-		setSelectedVideo(videos[0]);
-	}, [videos]);
-
-	return (
-		<div className="ui container">
-			<SearchBar onFormSubmit={search} />
-			<div className="ui grid">
-				<div className="ui row">
-					<div className="eleven wide column">
-						<VideoDetail video={selectedVideo} />
-					</div>
-					<div className="five wide column">
-						<VideoList onVideoSelect={setSelectedVideo} videos={videos} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+const createPolicy = (name, amount) => {
+	return {
+		type: "CREATE_POLICY",
+		payload: {
+			name: name,
+			amount: amount,
+		},
+	};
 };
 
-export default App;
+const deletePolicy = (name) => {
+	return {
+		type: "DELETE_POLICY",
+		payload: {
+			name: name,
+		},
+	};
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+	return {
+		type: "CREATE_CLAIM",
+		payload: {
+			name: name,
+			amountOfMoneyToCollection: amountOfMoneyToCollect,
+		},
+	};
+};
+
+// Reducers (departments)
+
+const claimHistory = (oldListOfClaims = [], action) => {
+	if (action.type === "CREATE_CLAIM") {
+		// this is important
+		return [...oldListOfClaims, action.payload];
+	}
+	return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+	if (action.type === "CREATE_CLAIM") {
+		return bagOfMoney - action.payload.amountOfMoneyToCollect;
+	} else if (action.type === "CREATE_POLICY") {
+		return bagOfMoney + action.payload.amount;
+	}
+	return bagOfMoney;
+};
+const policies = (listOfPolicies = [], action) => {
+	if (action.type === "CREATE_POLICY") {
+		return [...listOfPolicies, action.payload.name];
+	} else if (action.type === "DELETE_POLICY") {
+		return listOfPolicies.filter((name) => name !== action.payload.name);
+	}
+	return listOfPolicies;
+};
+
+const { createStore, combineReducers } = Redux;
+const ourDepartments = combineReducers({
+	accounting: accounting,
+	claimHistory: claimHistory,
+	policies: policies,
+});
+
+const store = createStore(ourDepartments);
+
+store.dispatch(createPolicy("alex", 20));
+store.dispatch(createPolicy("Gert", 30));
+store.dispatch(createPolicy("Svend", 40));
+
+console.log(store.getState());
