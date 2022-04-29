e506d41e6d89bb7848e1731e2fab7fab7238f2a8
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-04-29 10:57:51 +0200

    finished vanilla routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Accordion from "./Accordion";
 import Dropdown from "./Dropdown";
 import Search from "./Search";
 import Translate from "./Translate";
+import Route from "./Route";
+import Header from "./Header";
 
 const items = [
 	{
@@ -25,12 +27,30 @@ const options = [
 	{ label: "A Shade of Blue", value: "Blue" },
 ];
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 	const [selected, setSelected] = useState(options[0]);
 
 	return (
 		<div>
-			<Translate />
+			<Header />
+			<Route path="/">
+				<Accordion items={items} />
+			</Route>
+			<Route path="/list">
+				<Search />
+			</Route>
+			<Route path="/dropdown">
+				<Dropdown
+					label="Select a color"
+					options={options}
+					selected={selected}
+					onSelectedChange={setSelected}
+				/>
+			</Route>
+			<Route path="/translate">
+				<Translate />
+			</Route>
 		</div>
 	);
 };
