34b15fc1c7f0d3a4bd2d23e6e450ea8beef25598
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-04-29 11:22:23 +0200

    setup for refactor of video app

--- src/api/googleTranslate.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const KEY = "AIzaSyDfl-AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
-
-// GET https://www.googleapis.com/youtube/v3/search
-// Parameters snippet, maxResults 5, searchTerm -> surfing
-export default axios.create({
-	baseURL: "https://www.googleapis.com/youtube/v3",
-	params: { part: "snippet", type: "video", maxResults: 5, key: KEY },
-});
--- src/api/wikipedia.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// GET https://www.googleapis.com/youtube/v3/search
-// Parameters snippet, maxResults 5, searchTerm -> surfing
-export default axios.create({
-	baseURL:
-		"en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=",
-	params: { part: "snippet",  maxResults: 5 },
-});
