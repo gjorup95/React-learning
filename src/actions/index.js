eec445506608070eea21b3fdf1ce4a33cc426d96
Troels Hune Gjørup <troels.hune.gjoerup@post.au.dk>
2022-05-01 11:33:47 +0200

    refactored to use lodash chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,9 +15,11 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 	await dispatch(fetchPosts());
 
 	// LODASH
-	const userIds = _.uniq(_.map(getState().posts, "userId"));
-	userIds.forEach((id) => dispatch(fetchUser(id)));
-	
+	_.chain(getState().posts)
+		.map("userId")
+		.uniq()
+		.forEach((id) => dispatch(fetchUser(id)))
+		.value();
 };
 // Memoization
 /*
